0eda42118f0ec78e2a30e9d2710d1c51d25ad58a
Raphael Yoshiga <raphael.yoshiga@hotmail.com>
2020-06-10 23:33:02 +0100

    Implement prime generation by hand as per the test description

--- RYoshiga.PrimeNumbersTableGenerator/PrimeNumbersProvider.cs
+++ RYoshiga.PrimeNumbersTableGenerator/PrimeNumbersProvider.cs
@@ -1,16 +1,38 @@
 using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
 
 namespace RYoshiga.PrimeNumbersTableGenerator
 {
     public class PrimeNumbersProvider
     {
-        private readonly List<int> _primes;
+        private List<int> _primes;
 
         public PrimeNumbersProvider()
         {
-            _primes = SieveOfSundaram.GeneratePrimesSieveOfSundaram(1000);
+            NaivePrimeGeneration();
         }
-   
+
+        private void NaivePrimeGeneration()
+        {
+            _primes = new List<int>(new[] {2, 3});
+            for (int n = 5; _primes.Count < 1000; n += 2)
+            {
+                if (IsPrime(n))
+                    _primes.Add(n);
+            }
+        }
+
+        private bool IsPrime(int n)
+        {
+            foreach (var prime in _primes)
+            {
+                if (n % prime == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
         public int GetPrimeAt(in int index)
         {
             return _primes[index];
